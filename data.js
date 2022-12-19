81ea8f88604e51a7c2591db46cdeb16dd896ea89
jimi-chhatrala <jimichhatrala8488@gmail.com>
2022-12-19 22:11:49 +0530

    Guess the number & Index Home page added

---
+++ data.js
@@ -0,0 +1,27 @@
+const rows = [
+  {
+    id: 1,
+    name: 'Expanding Cards',
+    link: 'https://jimi-chhatrala.github.io/javascript-projects/Expanding Cards/',
+  },
+  {
+    id: 2,
+    name: 'Hamburger Menu',
+    link: 'https://jimi-chhatrala.github.io/javascript-projects/Hamburger Menu/',
+  },
+  {
+    id: 3,
+    name: 'Progress Steps',
+    link: 'https://jimi-chhatrala.github.io/javascript-projects/Progress Steps/',
+  },
+  {
+    id: 4,
+    name: 'Movie Seat Booking DOM and Local Storage',
+    link: 'https://jimi-chhatrala.github.io/javascript-projects/Movie Seat Booking DOM and Local Storage',
+  },
+  {
+    id: 5,
+    name: 'Guess the Number',
+    link: 'https://jimi-chhatrala.github.io/javascript-projects/Guess the Number',
+  },
+];
